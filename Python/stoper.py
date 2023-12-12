62a6cc0a5914959b8056543bb787d543f9897ed4
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2023-12-12 13:45:39 +0100

    Update stoper.py

--- Python/stoper.py
+++ Python/stoper.py
@@ -5,11 +5,17 @@ print("Rozpoczeto\n")
 start = time.time()
 last = start
 c = 1
+
 try:
-    while(1):
+    while True:
         input()
-        print("Okrazenie: " + str(c) + "\nCzas calkowity: " + str(round(time.time()-start, 2)) + "\nCzas okrazenia: " + str(round(time.time()-last, 2)) + "\n")
+        lap_time = round(time.time() - last, 2)
+        total_time = round(time.time() - start, 2)
+        lap_info = f"Okrazenie: {c}".ljust(20) + f"Czas calkowity: {total_time}".ljust(30) + f"Czas okrazenia: {lap_time}".ljust(25)
+        print(lap_info + "\n")
+
         last = time.time()
         c += 1
+
 except KeyboardInterrupt:
     print("Zakonczono program")
